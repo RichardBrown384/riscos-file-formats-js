201b7756b90792cadbc08837a9f748da6528e4fc
Ricki Brown <ricki.w.brown@gmail.com>
2022-08-16 11:44:29 +0200

    Separate out map functions.

--- src/draw/mapper/index.js
+++ src/draw/mapper/index.js
@@ -1,38 +1,16 @@
 const { Png, Base64 } = require('riscos-support');
 const { Sprite } = require('../../sprite');
 
-const { extractBitField } = require('../../common/bitwise');
 const MergingBoundingBox = require('./merging-bounding-box');
 
 const Constants = require('../constants');
+const mapPathElements = require('./path-elements-map');
+const mapPathAttributes = require('./path-attributes-map');
 
 const DRAW_UNITS_TO_USER_UNITS = (1.0 / 640.0) * (4.0 / 3.0);
 const DRAW_UNITS_PER_INCH = 180 * 256;
 
 const FIXED_POINT_CONVERSION_FACTOR = 65536.0;
-
-const MIN_STROKE_WIDTH = 160;
-
-const TAG_MAP = {
-  [Constants.PATH_TAG_END]: '',
-  [Constants.PATH_TAG_MOVE]: 'M',
-  [Constants.PATH_TAG_CLOSE_SUB_PATH]: 'Z',
-  [Constants.PATH_TAG_BEZIER]: 'C',
-  [Constants.PATH_TAG_LINE]: 'L',
-};
-
-const JOIN_MAP = {
-  [Constants.JOIN_MITRE]: 'mitre',
-  [Constants.JOIN_ROUND]: 'round',
-  [Constants.JOIN_BEVEL]: 'bevel',
-};
-
-const CAP_MAP = {
-  [Constants.CAP_BUTT]: 'butt',
-  [Constants.CAP_ROUND]: 'round',
-  [Constants.CAP_SQUARE]: 'square',
-  [Constants.CAP_TRIANGLE]: 'triangle',
-};
 
 function mapTransform(transform) {
   return [
@@ -41,83 +19,13 @@ function mapTransform(transform) {
   ];
 }
 
-function mapPathData(elements) {
-  const path = [];
-  for (let i = 0; i < elements.length; i += 1) {
-    const { tag, points = [] } = elements[i];
-    path.push(TAG_MAP[tag]);
-    path.push(points.flatMap(({ x, y }) => [x, y]).join(','));
-  }
-  return path.join('');
-}
-
-function mapColour(colour) {
-  if (colour === 0xFFFFFFFF) {
-    return 'none';
-  }
-  const elements = [
-    extractBitField(colour, 8, 8),
-    extractBitField(colour, 16, 8),
-    extractBitField(colour, 24, 8),
-  ];
-  return `rgb(${elements})`;
-}
-
-function mapStrokeWidth(outlineWidth) {
-  return Math.max(MIN_STROKE_WIDTH, outlineWidth);
-}
-
-function mapJoin(join) {
-  return join !== Constants.JOIN_MITRE && { 'stroke-linejoin': JOIN_MAP[join] };
-}
-
-function mapCapStart(capStart) {
-  return capStart in [Constants.CAP_SQUARE, Constants.CAP_ROUND]
-        && { 'stroke-linecap': CAP_MAP[capStart] };
-}
-
-function mapWindingRule(windingRule) {
-  return windingRule === Constants.WINDING_EVEN_ODD
-      && { 'fill-rule': 'evenodd' };
-}
-
-function mapStrokeDashoffset(strokeDashoffset) {
-  return strokeDashoffset && { 'stroke-dashoffset': strokeDashoffset };
-}
-
-function mapStrokeDashArray(strokeDasharray) {
-  return strokeDasharray && { 'stroke-dasharray': strokeDasharray };
-}
-
 function mapPathObject(pathObject) {
-  const {
-    fillColour,
-    outlineColour,
-    outlineWidth,
-    pathStyle: {
-      join,
-      capStart,
-      windingRule,
-      dash,
-    },
-    path,
-  } = pathObject;
-  const {
-    offset: strokeDashoffset,
-    array: strokeDasharray,
-  } = dash || {};
+  const { path, ...attributes } = pathObject;
   return {
     tag: 'path',
     attributes: {
-      d: mapPathData(path),
-      fill: mapColour(fillColour),
-      stroke: mapColour(outlineColour),
-      'stroke-width': mapStrokeWidth(outlineWidth),
-      ...(mapJoin(join)),
-      ...(mapCapStart(capStart)),
-      ...(mapWindingRule(windingRule)),
-      ...(mapStrokeDashoffset(strokeDashoffset)),
-      ...(mapStrokeDashArray(strokeDasharray)),
+      d: mapPathElements(path),
+      ...(mapPathAttributes(attributes)),
     },
   };
 }
---
+++ src/draw/mapper/path-elements-map.js
@@ -0,0 +1,21 @@
+const Constants = require('../constants');
+
+const TAG_MAP = {
+  [Constants.PATH_TAG_END]: '',
+  [Constants.PATH_TAG_MOVE]: 'M',
+  [Constants.PATH_TAG_CLOSE_SUB_PATH]: 'Z',
+  [Constants.PATH_TAG_BEZIER]: 'C',
+  [Constants.PATH_TAG_LINE]: 'L',
+};
+
+function mapPathElements(elements) {
+  const path = [];
+  for (let i = 0; i < elements.length; i += 1) {
+    const { tag, points = [] } = elements[i];
+    path.push(TAG_MAP[tag]);
+    path.push(points.flatMap(({ x, y }) => [x, y]).join(','));
+  }
+  return path.join('');
+}
+
+module.exports = mapPathElements;
